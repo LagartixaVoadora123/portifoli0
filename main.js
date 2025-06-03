d0cda6367432f9f8b6c00379d785be3b9b9d3044
LagartixaVoadora123 <146099834+LagartixaVoadora123@users.noreply.github.com>
2025-06-02 22:48:45 -0300

    .

--- main.js
+++ main.js
@@ -3,25 +3,22 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
 
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const agora = new Date();
 
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+const tempoObjetivo1 = new Date(agora.getTime() + (300 * 24 * 60 * 60 * 1000) + (38 * 60 * 60 * 1000) + (25 * 60 * 1000) + (32 * 1000));
+const tempoObjetivo2 = new Date(agora.getTime() + (228 * 24 * 60 * 60 * 1000) + (23 * 60 * 60 * 1000) + (2 * 60 * 1000) + (10 * 1000));
+const tempoObjetivo3 = new Date(agora.getTime() + (32 * 24 * 60 * 60 * 1000) + (22 * 60 * 60 * 1000) + (54 * 60 * 1000) + (2 * 1000));
 
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3];
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -34,6 +31,7 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
+
     if (tempoFinal > 0) {
         return [dias, horas, minutos, segundos];
     } else {
@@ -42,7 +40,7 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro() {
-    for (let i = 0; i < contadores.length; i++) {
+    for (let i = 0; i < tempos.length; i++) {
         document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
         document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
         document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
